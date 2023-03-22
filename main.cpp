a54cb4a067e363c4bfe0c1ca97264c22b85d4453
Polina <polinavinograd40@gmail.com>
2023-03-22 19:25:16 +0300

    splitted main function, added report

--- main.cpp
+++ main.cpp
@@ -11,6 +11,90 @@
 #include <vector>
 #include <string>
 
+void testing_mode() {
+  while (true) {
+    std::cout << "-----------------------------------\n";
+    std::cout << "1. Add formula to testing database\n2. Test\n3. back\n";
+    std::string choice1;
+    std::cin >> choice1;
+    std::cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
+    if (choice1 == "3") {
+      break;
+    } else if (choice1 == "1") {
+      std::ofstream file_o("../../formulas.txt", std::ios::app);
+      std::string addingFormula;
+      std::cout << "-----------------------------------\n";
+      std::cout << "Enter formula:\n";
+      std::getline(std::cin, addingFormula);
+      if (file_o.is_open()) {
+        file_o << addingFormula << std::endl;
+      }
+      file_o.close();
+    } else if (choice1 == "2") {
+      std::ifstream file("../../formulas.txt");
+      std::cout << "----------------------------------------------------------------\n";
+      std::cout << "Whether the string is a reduced propositional logic "
+                    "formula?\nWrite the answer in the following format:\n"
+                    "y - is a formula\nn - is not a formula\n";
+      if (file.is_open()) {
+        std::string line;
+        std::vector<bool> answerVector;
+        while (std::getline(file, line)) {
+          std::cout << line << std::endl;
+          std::string answer;
+          std::cin >> answer;
+          Parser parser;
+          auto formula = parser.parseFormula(line);
+          if ((formula && answer == "y") || (!formula && answer == "n")) {
+            std::cout << "True\n";
+            answerVector.push_back(1);
+          } else {
+            std::cout << "False\n";
+            answerVector.push_back(0);
+          }
+        }
+        int countResult = 0;
+        for (int i = 0; i < answerVector.size(); i++) {
+          if (answerVector[i] == 1) {
+            countResult++;
+          }
+        }
+        std::cout << "Result: " << countResult << "/" << answerVector.size()
+                  << std::endl;
+        file.close();
+      }
+    } else {
+      std::cout << "Unexpected answer!\n";
+    }
+  }
+}
+
+void checking_statements_mode() {
+  while (true) {
+    std::cout << "-----------------------------------\n";
+    std::cout << "2. back\nEnter the statement: \n";
+    std::string str;
+    std::getline(std::cin, str);
+    if (str == "2") {
+      break;
+    } else {
+      if (str != "")
+      {
+        Parser parser;
+        auto formula = parser.parseFormula(str);
+        if (formula) {
+          std::cout << formula->toString() << " is a formula" << std::endl;
+        } else {
+          std::cout << "NO\n";
+        }
+      }
+      else{
+        std::cout << "NO\n";
+      }
+    }
+  }
+}
+
 
 int main() {
   
@@ -25,90 +109,10 @@ int main() {
     std::cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
     if (choice == "3") {
       return 0;
-
     } else if (choice == "1") {
-      while (true) {
-        std::cout << "-----------------------------------\n";
-        std::cout << "1. Add formula to testing database\n2. Test\n3. back\n";
-        std::string choice1;
-        std::cin >> choice1;
-        std::cin.ignore( std::numeric_limits<std::streamsize>::max(), '\n' );
-        if (choice1 == "3") {
-          break;
-        } else if (choice1 == "1") {
-          std::ofstream file_o("../../formulas.txt", std::ios::app);
-          std::string addingFormula;
-          std::cout << "-----------------------------------\n";
-          std::cout << "Enter formula:\n";
-          std::getline(std::cin, addingFormula);
-          //std::cin >> addingFormula;
-          if (file_o.is_open()) {
-            file_o << addingFormula << std::endl;
-          }
-          file_o.close();
-        } else if (choice1 == "2") {
-          std::ifstream file("../../formulas.txt");
-          std::cout << "----------------------------------------------------------------\n";
-          std::cout << "Whether the string is a reduced propositional logic "
-                       "formula?\nWrite the answer in the following format:\n"
-                       "y - is a formula\nn - is not a formula\n";
-          if (file.is_open()) {
-            std::string line;
-            std::vector<bool> answerVector;
-            while (std::getline(file, line)) {
-              std::cout << line << std::endl;
-              std::string answer;
-              std::cin >> answer;
-              Parser parser;
-              auto formula = parser.parseFormula(line);
-              if ((formula && answer == "y") || (!formula && answer == "n")) {
-                std::cout << "True\n";
-                answerVector.push_back(1);
-              } else {
-                std::cout << "False\n";
-                answerVector.push_back(0);
-              }
-            }
-            int countResult = 0;
-            for (int i = 0; i < answerVector.size(); i++) {
-              if (answerVector[i] == 1) {
-                countResult++;
-              }
-            }
-            std::cout << "Result: " << countResult << "/" << answerVector.size()
-                      << std::endl;
-            file.close();
-          }
-        } else {
-          std::cout << "Unexpected answer!\n";
-        }
-      }
+      testing_mode();
     } else if (choice == "2") {
-      while (true) {
-        std::cout << "-----------------------------------\n";
-        std::cout << "2. back\nEnter the statement: \n";
-        std::string str;
-        std::getline(std::cin, str);
-
-        // std::cin >> str;
-        if (str == "2") {
-          break;
-        } else {
-          if (str != "")
-          {
-            Parser parser;
-            auto formula = parser.parseFormula(str);
-            if (formula) {
-              std::cout << formula->toString() << " is a formula" << std::endl;
-            } else {
-              std::cout << "NO\n";
-            }
-          }
-          else{
-            std::cout << "NO\n";
-          }
-        }
-      }
+      checking_statements_mode();
     } else {
       std::cout << "Unexpected answer!\n";
     }
